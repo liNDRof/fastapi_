d84a0672b7f7bdada4e76fd93f902ccc2b07ef8e
liNDRof <156926880+liNDRof@users.noreply.github.com>
2025-02-02 21:18:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import users, teams, tournaments
+from routers import users, teams, tournaments, notifications
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(teams.router)
 app.include_router(tournaments.router)
+app.include_router(notifications.router)
 
 @app.get("/")
 async def start():
