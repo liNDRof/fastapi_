40924ac49fc46c03d212c619c2bb16172a91df54
liNDRof <156926880+liNDRof@users.noreply.github.com>
2025-02-03 15:40:58 +0100

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -2,42 +2,46 @@ from fastapi import APIRouter, HTTPException
 from models import User
 from schemas import UserPydantic
 from typing import List
-import uuid
 
 router = APIRouter()
 
 users: List[User] = []
 
+current_id = 1
 
 @router.post("/user")
 async def create_user(user: UserPydantic):
+    global current_id
+
     if any(u.email == user.email for u in users):
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="емейл вже зареєстрований")
 
     new_user = User(
-        id=user.id,
+        id=current_id,
         name=user.name,
         email=user.email,
         passwort=user.password
     )
-    users.append(new_user)
-    return new_user
 
+    users.append(new_user)
+
+    current_id += 1
+
+    return new_user
 
 @router.get("/users")
 async def get_users():
     return users
 
 @router.get("/user/{user_id}")
-async def get_user(user_id: str):
+async def get_user(user_id: int):
     for user in users:
         if user.id == user_id:
             return user
-    raise HTTPException(status_code=404, detail="User not found")
-
+    raise HTTPException(status_code=404, detail="юзера не знайдено")
 
 @router.delete("/user/{user_id}")
-async def delete_user(user_id: str):
+async def delete_user(user_id: int):
     global users
     users = [u for u in users if u.id != user_id]
-    return {"detail": "User deleted"}
+    return {"detail": "юзер делітед"}
